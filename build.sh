de5ec9fd4ac69d7aa753ea0dffe0f97fb42b52cd
marius <marius.stoian@upb.ro>
2022-11-30 15:38:20 +0530

    fix cmake

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-export LD_LIBRARY_PATH=/usr/local/bin/:/Users/marius/projects/protobuf/bazel-out/darwin_arm64-fastbuild/bin/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/Users/marius/projects/protobuf/bazel-out/darwin_arm64-fastbuild/bin/_objs/protobuf/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/bin/:/Users/marius/projects/protobuf/bazel-out/darwin_arm64-fastbuild/bin/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/Users/marius/projects/protobuf/bazel-out/darwin_arm64-fastbuild/bin/_objs/protobuf/:$LD_LIBRARY_PATH
+rm -rf build
 mkdir build
 cd build
 cmake ../
